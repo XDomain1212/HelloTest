83a7ed5fdf45b3b57fd583a7f8b88699ba5e2aad
Lee Min <19914014@qq.com>
2020-06-19 03:17:16 +0800

    updateCode

---
+++ test_file.py
@@ -0,0 +1,12 @@
+def get_hello(name:str) -> str:
+    """
+    This is a hello function 
+    
+    name is string type 
+
+    it's return a string type 
+    """
+    print('Hello ' + name)
+
+
+get_hello()
